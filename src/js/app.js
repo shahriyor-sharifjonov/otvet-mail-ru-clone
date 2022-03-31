76b710c4acad71dc084092dfadd98d492ca33e9c
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-03-31 22:45:05 +0500

    header

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,43 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+import $ from 'jquery';
+
+const headerButton = document.querySelector(".header__button");
+const headerMenu = document.querySelector(".header__menu");
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+};
+
+headerButton.onclick = menuToggle;
+
+window.onclick = (e) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle();
+};
+
+$(document).ready(function() {
+    $(".accordion > .accordion__button").on("click", function() {
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this)
+                .siblings(".accordion__content")
+                .slideUp(200);
+        } else {
+            $(".accordion > .accordion__button").removeClass("active");
+            $(this).addClass("active");
+            $(".accordion__content").slideUp(200);
+            $(this)
+                .siblings(".accordion__content")
+                .slideDown(200);
+        }
+    });
+});
